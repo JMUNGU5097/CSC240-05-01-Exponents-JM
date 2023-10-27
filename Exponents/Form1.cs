262b34b5987f22020b36cf54ceacaebe44c88871
Joselyn Munguia Mejia <JMUNGU5097@my.sullivan.edu>
2023-10-27 15:24:53 -0400

    completed steps 1-8.

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 /**************************************
- * Name:
- * Date:
+ * Name: JOSELYN MUNGUIA MEJIA
+ * Date: 10-27-2023
  * Description: A program using user-created method
  *              for the math function Square and Cube
  * Assistance:
@@ -27,38 +27,53 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
+            int UserInput;
 
             // INPUT
+            UserInput = Convert.ToInt32(xInputTextBox.Text);
 
             // PROCESS
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
+            xOutputLabel.Text = "The square of " + UserInput + " is " + (UserInput * UserInput).ToString() + ".";
+            
 
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
+            xOutputLabel.Text += "\nThe cube of " + UserInput + " is " + (UserInput * UserInput * UserInput).ToString() + ".";
+            xOutputLabel.Visible = true;
 
             // OUTPUT
             // set the output to visible
+            xOutputLabel.Visible = true;
 
             // turn the Go button off
+            xGoButton.Enabled = false;
         }
 
         public int Square(int num)
         {
             // we will create this code in class
+            return 0;
         }
 
         public int Cube(int num)
         {
             // we will create this code in class
+            return 0;
         }
 
         private void xResetButton_Click(object sender, EventArgs e)
         {
             // the input and output text should be emptied
+            xInputTextBox.Text = " ";
+
             // turn the output to invisible
+            xOutputLabel.Visible = false;
+
             // turn the Go button back on
+            xGoButton.Enabled = true;
         }
 
     }
